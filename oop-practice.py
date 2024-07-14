ce9e145331e7b11d33f099b35974f83a98f6d731
MouadhBNB7741 <129070196+MouadhBNB7741@users.noreply.github.com>
2024-07-14 15:54:11 +0100

    the queue class

--- oop-practice.py
+++ oop-practice.py
@@ -1,19 +1,41 @@
 class Stack():
-    def __init__(self,arr:list):
+    def __init__(self,arr:list=[]):
         self.items=arr
     
     def push(self,item):
         self.items.append(item)
     
     def peek(self):
-        return self.items[len(self.items)-1]
+        return self.items[-1]
     
     def isEmpty(self):
         return len(self.items)==0
     
     def pop(self):
-        self.items.pop()
-    
+        if(not self.isEmpty):
+            return self.items.pop()
+        return None
+        
     def size(self):
         return len(self.items)
     
+class Queue:
+    def __init__(self,arr:list):
+        self.items=arr
+
+    def enqueue(self,item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if(not self.isEmpty):
+            return self.items.popleft()
+        return None
+
+    def isEmpty(self):
+        return len(self.items)==0
+    
+    def peek(self):
+        return self.items[0]
+    
+    def size(self):
+        return len(self.items)
